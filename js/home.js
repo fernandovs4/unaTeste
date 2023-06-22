422c3c6c7b91c4c7b8c0a5dce78d34039ffe48fa
FernandoUna <fernando@unacapital.com.br>
2023-06-22 11:33:46 -0300

    mudando home.js

--- js/home.js
+++ js/home.js
@@ -4,12 +4,18 @@ fetch('https://earnest-torte-8a0636.netlify.app/cota.json').
     let resposta = response
     resposta = resposta['dados'];
     let dados = []
-    for(let i = 0; i < resposta['Cota'].length; i++){
-        dados.push([resposta['Cota'][i], resposta['Mes'][i], resposta['Ano'][i], resposta['12 Meses'][i], resposta['24 Meses'][i], resposta['36 Meses'][i], resposta['60 Meses'][i]])
+    for(let i = 0; i < resposta['Mes'].length; i++){
+        if (i == 0){
+            dados.push([resposta['Cota'][i], resposta['Mes'][i], resposta['Ano'][i], resposta['12 Meses'][i], resposta['24 Meses'][i], resposta['36 Meses'][i], resposta['60 Meses'][i]])
+        }else{
+            dados.push([resposta['Mes'][i], resposta['Ano'][i], resposta['12 Meses'][i], resposta['24 Meses'][i], resposta['36 Meses'][i], resposta['60 Meses'][i]])
+        }
     }
         for(let i = 0; i < 7; i++){
-            let div = document.getElementById(`${i + 1}`);
-            div.innerHTML = dados[0][i];
+            if (i == 0){
+                let div = document.getElementById(`${i + 1}`);
+                div.innerHTML = dados[0][i];
+            }
             let div2 = document.getElementById(`${i + 11}`);
             div2.innerHTML = dados[1][i];
             let div3 = document.getElementById(`${i + 21}`);
